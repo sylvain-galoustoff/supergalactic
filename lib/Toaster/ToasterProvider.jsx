c5d31b433e68222e3e72e46232bee770f411a70e
Sylvain-MED <sylvain.galoustoff@gmail.com>
2024-07-13 10:42:25 +0200

    Export toasterSlice

--- lib/Toaster/ToasterProvider.jsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import { Provider } from "react-redux";
-import { store } from "./redux/store";
-
-const ToasterProvider = ({ children }) => {
-  return <Provider store={store}>{children}</Provider>;
-};
-
-export default ToasterProvider;
--- lib/main.jsx
+++ lib/main.jsx
@@ -1,4 +1,5 @@
 export { default as DataSearch } from "./DataSearch/DataSearch";
 export { default as DataTable } from "./DataTable/DataTable";
 export { default as Toaster } from "./Toaster/Toaster";
-export { default as ToasterProvider } from "./Toaster/ToasterProvider";
+export { default as toasterSlice } from "./Toaster/redux/toasterSlice";
+export { useToast } from "./Toaster/hooks";
