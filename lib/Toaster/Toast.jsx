0e5e57e457fa1932db39de1cb746d6e228e2a10d
Sylvain-MED <sylvain.galoustoff@gmail.com>
2024-07-13 16:42:25 +0200

    Add Toast remove after 5s delay

--- lib/Toaster/Toast.jsx
+++ lib/Toaster/Toast.jsx
@@ -1,9 +1,18 @@
 import style from "./Toaster.module.scss";
 import { IoClose } from "react-icons/io5";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 function Toast({ data, animation, onRemoveToast }) {
   const [removing, setRemoving] = useState(false);
+
+  useEffect(() => {
+    setTimeout(() => {
+      setRemoving(true);
+      setTimeout(() => {
+        onRemoveToast(data.id);
+      }, 300);
+    }, 5000);
+  }, []);
 
   const deleteToast = () => {
     setRemoving(true);
