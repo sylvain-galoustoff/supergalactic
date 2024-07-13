676e9847b0a8e933d5cee444189e226459f208c3
Sylvain-MED <sylvain.galoustoff@gmail.com>
2024-07-13 11:54:50 +0200

    Add remove Toast button

--- lib/Toaster/Toast.jsx
+++ lib/Toaster/Toast.jsx
@@ -1,9 +1,21 @@
+import { useDispatch } from "react-redux";
 import style from "./Toaster.module.scss";
+import { IoClose } from "react-icons/io5";
+import { deleteToast } from "./redux/toasterSlice";
 
 function Toast({ data, animation }) {
+  const dispatch = useDispatch();
+
+  const removeToast = () => {
+    dispatch(deleteToast(data));
+  };
+
   return (
     <div className={`${style.toast} ${data.type} ${style[animation]} toast`}>
-      <p className={`${style.header} toast-header`}>Header message</p>
+      <div className={`${style.header} toast-header`}>
+        <p>Header message</p>
+        <IoClose onClick={removeToast} />
+      </div>
       <p className={`${style.body} toast-body`}>{data.message}</p>
     </div>
   );
