e32a2c41422723d39b2cc9e7ea5ac50df3e8f2df
Sylvain-MED <sylvain.galoustoff@gmail.com>
2024-07-13 10:45:26 +0200

    Removed store from Toaster component

--- lib/Toaster/Toaster.jsx
+++ lib/Toaster/Toaster.jsx
@@ -1,10 +1,7 @@
-import { useSelector } from "react-redux";
 import Toast from "./Toast";
 import style from "./Toaster.module.scss";
 
-function Toaster({ toasterId }) {
-  const toastsList = useSelector((state) => state.toaster.toasts);
-
+function Toaster({ toastsList, toasterId }) {
   const renderToast = Object.keys(toastsList).map((key) => <Toast key={key} data={toastsList[key]} />);
 
   return (
