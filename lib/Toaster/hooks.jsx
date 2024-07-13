1647cd9753fc329b7b4e0cd2804aebff595ce09b
Sylvain-MED <sylvain.galoustoff@gmail.com>
2024-07-13 14:09:18 +0200

    Removed hook based on redux store

--- lib/Toaster/hooks.jsx
+++ lib/Toaster/hooks.jsx
@@ -1,12 +1,1 @@
-import { useDispatch } from "react-redux";
-import { addToast } from "./redux/toasterSlice";
 
-export const useToast = (newToast) => {
-  const dispatch = useDispatch();
-
-  const addToastToToaster = (newToast) => {
-    dispatch(addToast(newToast));
-  };
-
-  return addToastToToaster;
-};
