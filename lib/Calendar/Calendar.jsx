d9c1f69eafcea155dc0c8906387afae59961baed
Sylvain-MED <sylvain.galoustoff@gmail.com>
2024-07-21 16:55:29 +0200

    Add year and month controlled inputs

--- lib/Calendar/Calendar.jsx
+++ lib/Calendar/Calendar.jsx
@@ -2,13 +2,36 @@ import { useState } from "react";
 import style from "./Calendar.module.scss";
 
 function Calendar() {
-  const [currentDate, setCurrentDate] = useState(new Date());
+  const today = new Date();
+  const [month, setMonth] = useState(today.getMonth().toString());
+  const [year, setYear] = useState(today.getFullYear().toString());
+
+  const changeMonth = (e) => {
+    setMonth(e.target.value.toString());
+  };
+
+  const changeYear = (e) => {
+    setYear(e.target.value.toString());
+  };
 
   return (
     <div className={`${style.calendar}`}>
       <div className={`${style.selectors}`}>
-        <span className={`${style.monthSelector}`}>{currentDate.getMonth()}</span>
-        <span className={`${style.yearSelector}`}>{currentDate.getFullYear()}</span>
+        <select className={`${style.monthSelector}`} value={month} onChange={changeMonth}>
+          <option value="0">Janvier</option>
+          <option value="1">Février</option>
+          <option value="2">Mars</option>
+          <option value="3">Avril</option>
+          <option value="4">Mai</option>
+          <option value="5">Juin</option>
+          <option value="6">Juillet</option>
+          <option value="7">Août</option>
+          <option value="8">Septembre</option>
+          <option value="9">Octobre</option>
+          <option value="10">Novembre</option>
+          <option value="11">Décembre</option>
+        </select>
+        <input type="number" step="1" className={`${style.yearSelector}`} value={year} onChange={changeYear} />
       </div>
       <div className={`${style.days} ${style.grid}`}>
         <div className={`${style.day} ${style.weekday}`}>Lundi</div>
